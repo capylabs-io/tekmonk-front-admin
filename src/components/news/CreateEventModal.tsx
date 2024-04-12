5c316c4c80368a5627eeaea34a3153f3a38abfe0
thai <nxthai23@gmail.com>
2024-04-12 16:25:38 +0700

    feat: add image preview when create news/event/hiring

--- src/components/news/CreateEventModal.tsx
+++ src/components/news/CreateEventModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Select,
   SelectContent,
@@ -6,16 +6,26 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/common/Select";
+import Image from "next/image";
 import { Input } from "../common/Input";
 import classNames from "classnames";
 import { Textarea } from "../common/TextArea";
 import { Inputcn } from "../common/Inputcn";
+import { ImagePlus } from "lucide-react";
 
 type Props = {
   customClassname?: string;
 };
 
 export const CreateEventModal = ({ customClassname }: Props) => {
+  const [selectedFile, setSelectedFile] = useState();
+
+  const handleChangeFile = (e: any) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setSelectedFile(e.target.files[0]);
+    }
+  };
+
   return (
     <div className={classNames("flex flex-col gap-y-4", customClassname)}>
       <div className="grid grid-cols-5">
@@ -30,8 +40,25 @@ export const CreateEventModal = ({ customClassname }: Props) => {
       </div>
       <div className="grid grid-cols-5">
         <div className="col-span-2 my-auto">Thumbnail</div>
-        <div className="col-span-3">
-          <Inputcn type="file"></Inputcn>
+        <div className="col-span-3 flex gap-x-2">
+          <div className="border rounded-lg h-full min-w-[48px] flex justify-center">
+            {!selectedFile ? (
+              <ImagePlus className="flex justify-center my-auto" />
+            ) : (
+              <Image
+                className="rounded-md object-cover"
+                src={URL.createObjectURL(selectedFile)}
+                alt="image"
+                width={48}
+                height={48}
+              />
+            )}
+          </div>
+          <Inputcn
+            type="file"
+            accept="images/*"
+            onChange={handleChangeFile}
+          ></Inputcn>
         </div>
       </div>
       <div className="grid grid-cols-5">
