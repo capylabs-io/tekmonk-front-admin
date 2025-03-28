e26ced9c826f5484a7d0c58d0e84687c01bb3eb6
hieuvu0211 <learncnnt@gmail.com>
2025-03-28 14:22:01 +0700

    feat: implement layout and page structure for mission and achievement management
    - Added new layout components for mission and achievement pages with role-based access control.
    - Refactored page components to utilize new Tabs component for better navigation between system and manual configurations.
    - Enhanced data fetching for missions and achievements using React Query, improving performance and user experience.
    - Updated table structures to display relevant information and actions for missions and achievements.

--- src/app/quan-ly/nhiem-vu/page.tsx
+++ src/app/quan-ly/nhiem-vu/page.tsx
@@ -1,31 +1,19 @@
 "use client";
 
-import { CommonButton } from "@/components/common/button/CommonButton";
 import { Edit, PanelLeft } from "lucide-react";
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/common/Tabs";
 import { CommonCard } from "@/components/common/CommonCard";
 import "react-quill/dist/quill.snow.css";
 import { CommonTable } from "@/components/common/CommonTable";
 import { ColumnDef } from "@tanstack/react-table";
 import { AchievementFormData } from "@/components/achievement/CreateAchievementModal";
-import { Input } from "@/components/common/Input";
-import { useMemo, useState } from "react";
-import {
-  CreateMissionModal,
-  MisionFormData,
-} from "@/components/mission/CreateMissionModal";
-import { useMission } from "@/hooks/useMission";
-import { getMission, postMission, updateMission } from "@/requests/mission";
-import { useSnackbarStore } from "@/store/SnackbarStore";
-import { useLoadingStore } from "@/store/LoadingStore";
-import { Mission, MissionType } from "@/types/mission";
+import { useState } from "react";
+import { getMission } from "@/requests/mission";
 import { useQuery } from "@tanstack/react-query";
 import qs from "qs";
+import { Tabs } from "@/components/new/tabs";
+import { useMission } from "@/hooks/useMission";
+import { Mission } from "@/types/mission";
+import { Input } from "@/components/common/Input";
 
 export default function Page() {
   const {
@@ -38,30 +26,22 @@ export default function Page() {
     setPage,
     setIsOpenCreateModal,
   } = useMission();
-
-  const [searchQuery, setSearchQuery] = useState("");
-  const [textSearch, setTextSearch] = useState("");
-  const handleSearch = () => {
-    setSearchQuery(textSearch);
-  };
-  const [isEditing, setIsEditing] = useState(false);
-  const [currentMissionSelected, setCurrentMissionSelected] =
-    useState<Mission>();
-  const [showSuccess, showError] = useSnackbarStore((state) => [
-    state.success,
-    state.error,
-  ]);
-  const [showLoading, hideLoading] = useLoadingStore((state) => [
-    state.show,
-    state.hide,
-  ]);
+  const tabs = [
+    { id: "EverySession", label: "Thuộc hệ thống" },
+    { id: "Manual", label: "Cấu hình ngoài" },
+  ];
+  const [activeTab, setActiveTab] = useState(tabs[0]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemPerPage] = useState(10);
+
   const { data: missionList, refetch: refetchMissionList } = useQuery({
-    queryKey: ["missionList"],
+    queryKey: ["missionList", activeTab.id],
     queryFn: async () => {
       try {
         const queryString = qs.stringify({
+          filters: {
+            type: activeTab.id,
+          },
           populate: ["class", "teacher"],
           pagination: {
             page: currentPage,
@@ -75,68 +55,22 @@ export default function Page() {
     },
     refetchOnWindowFocus: false,
   });
-  const handlePostMission = async (data: MisionFormData) => {
-    try {
-      showLoading();
-      if (currentMissionSelected) {
-        const res = await updateMission(currentMissionSelected.id || 0, data);
-        if (res) {
-          showSuccess("Cập nhật", "Nhiệm vụ cập nhật thành công!");
-        }
-      } else {
-        const res = await postMission(data);
-        if (res) {
-          showSuccess("Tạo mới", "Nhiệm vụ tạo mới thành công!");
-        }
-      }
-    } catch (error) {
-      console.log("error", error);
-      if (currentMissionSelected) {
-        showError("Cập nhật", "Nhiệm vụ cập nhật thất bại!");
-      } else {
-        showError("Tạo mới", "Nhiệm vụ tạo mới thất bại!");
-      }
-    } finally {
-      hideLoading();
-      refetchMissionList();
-    }
-  };
-  const systemMissionList = useMemo(() => {
-    return missionList
-      ? missionList.data.filter(
-          (mission: Mission) => mission.type === MissionType.EVERY_SESSION
-        )
-      : [];
-  }, [missionList]);
-  const customMissionList = useMemo(() => {
-    return missionList
-      ? missionList.data.filter(
-          (mission: Mission) => mission.type === MissionType.MANUAL
-        )
-      : [];
-  }, [missionList]);
-  const columns: ColumnDef<AchievementFormData>[] = [
+
+  const columns: ColumnDef<Mission>[] = [
     {
       header: "STT",
       cell: ({ row }) => <span>{row.index + 1}</span>,
     },
     {
       header: "Tên nhiệm vụ",
-      cell: ({ row }) => (
-        <div
-          className="bg-center bg-no-repeat bg-cover h-[80px] rounded-xl w-[130px]"
-          style={{
-            backgroundImage: `url(${row.original?.icon})`,
-          }}
-        ></div>
-      ),
-    },
-    {
-      header: "Thuộc khoá học",
       cell: ({ row }) => <span>{row.original.title}</span>,
     },
     {
-      header: "Trạng thái",
+      header: "Mô tả",
+      cell: ({ row }) => <span>{row.original.description}</span>,
+    },
+    {
+      header: "Loại",
       cell: ({ row }) => <div>{row.original.type}</div>,
     },
     {
@@ -149,22 +83,11 @@ export default function Page() {
               className="p-2 hover:bg-gray-100 rounded-full"
               onClick={(e) => {
                 e.stopPropagation();
-                setIsEditing((prev) => (prev = true));
-                setIsOpenCreateModal(true);
                 // Add edit handler here
               }}
             >
               <Edit className="h-4 w-4" color="#7C6C80" />
             </button>
-            {/* <button
-                className="p-2 hover:bg-gray-100 rounded-full"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  // Add edit handler here
-                }}
-              >
-                <Trash2 className="h-4 w-4" color="#7C6C80" />
-              </button> */}
           </div>
         );
       },
@@ -173,8 +96,8 @@ export default function Page() {
 
   return (
     <>
-      <div className="w-full h-screen border-r border-gray-20">
-        <div className="w-full h-[68px] flex flex-col sm:flex-row items-start sm:items-center justify-between px-2 border-b border-gray-20">
+      <div className="w-full border-gray-20 overflow-hidden flex flex-col gap-y-4 border">
+        <div className="w-full h-[68px]  flex flex-col sm:flex-row items-start sm:items-center justify-between px-2 border-b border-gray-20">
           <div className="text-SubheadLg text-gray-95 mb-2 sm:mb-0 flex items-center justify-center gap-2">
             <CommonCard
               size="small"
@@ -185,98 +108,37 @@ export default function Page() {
             Nhiệm vụ
           </div>
         </div>
-        {/* <div className="w-full flex flex-col py-2">
-        <div className="h-9 w-[265px] flex items-center justify-center text-gray-95 gap-3"> */}
-        <Tabs defaultValue="system" className="w-full !h-[calc(100vh-68px)]">
-          <TabsList className="w-full border-b border-gray-200 !justify-start">
-            <TabsTrigger value="system">Thuộc hệ thống</TabsTrigger>
-            <TabsTrigger value="outside">Cấu hình ngoài</TabsTrigger>
-          </TabsList>
-          <TabsContent
-            value="system"
-            className="overflow-y-auto !h-[calc(100%-40px)] p-4"
-          >
-            <div className="w-full h-[calc(100%-40px-12px)] overflow-y-auto">
-              <div className="flex justify-between items-center">
-                <Input
-                  type="text"
-                  isSearch={true}
-                  value={textSearch}
-                  onChange={setTextSearch}
-                  placeholder="Tìm kiếm nhiệm vụ theo từ khoá"
-                  customClassNames="max-w-[410px] h-10 mb-4"
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      handleSearch();
-                    }
-                  }}
-                  onSearch={handleSearch}
-                />
-              </div>
-              <CommonTable
-                data={systemMissionList}
-                isLoading={false}
-                columns={columns}
-                page={page}
-                totalPage={totalPage}
-                totalDocs={totalDocs}
-                onPageChange={setPage}
-                docsPerPage={limit}
-                onPageSizeChange={setLimit}
-              />
-            </div>
-          </TabsContent>
-          <TabsContent
-            value="outside"
-            className="overflow-y-auto !h-[calc(100%-40px)] p-4"
-          >
-            <div className="w-full h-[calc(100%-40px-12px)] overflow-y-auto">
-              <div className="flex justify-between items-center">
-                <Input
-                  type="text"
-                  isSearch={true}
-                  value={textSearch}
-                  onChange={setTextSearch}
-                  placeholder="Tìm kiếm thành tích theo từ khoá"
-                  customClassNames="max-w-[410px] h-10 mb-4"
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      handleSearch();
-                    }
-                  }}
-                  onSearch={handleSearch}
-                />
-                <CommonButton
-                  variant="primary"
-                  className="h-9 !w-max px-6"
-                  onClick={() => setIsOpenCreateModal(true)}
-                >
-                  Tạo mới
-                </CommonButton>
-              </div>
-              <CommonTable
-                data={customMissionList}
-                isLoading={false}
-                columns={columns}
-                page={page}
-                totalPage={totalPage}
-                totalDocs={totalDocs}
-                onPageChange={setPage}
-                docsPerPage={limit}
-                onPageSizeChange={setLimit}
-              />
-            </div>
-          </TabsContent>
-        </Tabs>
+        <div className="flex items-center gap-x-4 border-b border-gray-20">
+          <Tabs
+            tabs={tabs}
+            currentTab={activeTab}
+            setCurrentTab={setActiveTab}
+            className="w-[265px] space-x-4"
+          />
+        </div>
+        <div className="flex flex-col gap-y-4 px-4 !h-[calc(100vh-68px-36px-40px-16px)]">
+          <Input
+            type="text"
+            placeholder="Tìm kiếm"
+            customClassNames="max-w-[320px]"
+          />
+
+          {missionList && (
+            <CommonTable
+              data={missionList.data}
+              isLoading={false}
+              columns={columns}
+              page={page}
+              totalPage={totalPage}
+              totalDocs={totalDocs}
+              onPageChange={setPage}
+              docsPerPage={limit}
+              onPageSizeChange={setLimit}
+              customTableClassname="!h-[calc(100%-50px)]"
+            />
+          )}
+        </div>
       </div>
-      <CreateMissionModal
-        isEdit={isEditing}
-        open={isOpenCreateModal}
-        onOpenChange={(value) => {
-          setIsOpenCreateModal(value);
-        }}
-        onSubmit={handlePostMission}
-      />
     </>
   );
 }
